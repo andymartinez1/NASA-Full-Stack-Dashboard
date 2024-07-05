d3f67f817ec9b47adc0983e3dc21f618c4452229
Andrew Martinez <andy72391@gmail.com>
2024-07-04 22:28:50 -0400

    Added functionaliy and validation for POST /launches requests in order to successfully launch missions. Implemented abort mission functionaliy. Rather than deleting the launch entirely, the upcoming and success flags are set to false.

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,9 @@
-const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
+const {
+  getAllLaunches,
+  addNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById,
+} = require("../../models/launches.model");
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
@@ -7,10 +12,48 @@ function httpGetAllLaunches(req, res) {
 function httpAddNewLaunch(req, res) {
   const launch = req.body;
 
+  if (
+    !launch.mission ||
+    !launch.rocket ||
+    !launch.launchDate ||
+    !launch.target
+  ) {
+    return res.status(400).json({
+      error: "Missing required launch property.",
+    });
+  }
+
   launch.launchDate = new Date(launch.launchDate);
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid launch date.",
+    });
+  }
 
   addNewLaunch(launch);
   return res.status(201).json(launch);
 }
 
-module.exports = { httpGetAllLaunches, httpAddNewLaunch };
+async function httpAbortLaunch(req, res) {
+  const launchId = Number(req.params.id);
+
+  const existsLaunch = await existsLaunchWithId(launchId);
+  if (!existsLaunch) {
+    return res.status(404).json({
+      error: "Launch not found",
+    });
+  }
+
+  const aborted = await abortLaunchById(launchId);
+  if (!aborted) {
+    return res.status(400).json({
+      error: "Launch not aborted",
+    });
+  }
+
+  return res.status(200).json({
+    ok: true,
+  });
+}
+
+module.exports = { httpGetAllLaunches, httpAddNewLaunch, httpAbortLaunch };
